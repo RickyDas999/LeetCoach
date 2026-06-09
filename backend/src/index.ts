5632edf43ecaa6babbfa724ae628c0bc1ef16a6d
RickyDas999 <116985003+RickyDas999@users.noreply.github.com>
2026-06-09 17:00:56 -0400

    Create routing stubs with placeholders

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,70 @@
 export const handler = async (event: any) => {
+    const method = event.requestContext.http.method;
+    const path = event.requestContext.http.path;
+
+    if (method === "GET" && path === "/problems") {
+        return getProblems();
+    }
+
+    if (method === 'POST' && path === "/problems") {
+        return createProblem();
+    } 
+
+    if (method === "GET" && path.startsWith("/problems/")) {
+        const slug = path.split("/")[2]
+        return getProblem(slug);
+    }
+
+    if (method === "POST" && path === "/attempts") {
+        return createAttempt();
+    }
+
+    if (method === "GET" && path === "/reviews/today") {
+        return getTodayReviews();
+    }
+
+    if (method === "GET" && path === "/reviews/upcoming") {
+        return getUpcomingReviews();
+    }
+
     return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "LeetCoach API is running" })
+        statusCode: 404,
+        body: JSON.stringify({ error: "Not found"})
+    }
+}
+
+async function createProblem() {
+    return {statusCode: 200,
+        body: JSON.stringify({message: "To do"})
+    }
+}
+
+async function getProblems() {
+    return {statusCode: 200,
+        body: JSON.stringify({message: "To do"})
+    }
+}
+
+async function getProblem(slug: string) {
+    return {statusCode: 200,
+        body: JSON.stringify({message: "To do"})
+    }
+}
+
+async function createAttempt() {
+    return {statusCode: 200,
+        body: JSON.stringify({message: "To do"})
+    }
+}
+
+async function getTodayReviews() {
+    return {statusCode: 200,
+        body: JSON.stringify({message: "To do"})
+    }
+}
+
+async function getUpcomingReviews() {
+    return {statusCode: 200,
+        body: JSON.stringify({message: "To do"})
     }
 }
